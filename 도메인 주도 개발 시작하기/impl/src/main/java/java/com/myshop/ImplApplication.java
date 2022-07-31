7d8e344131d9516a9f2c7419514f77e67f65d7b8
KimBeomJin <sdpthf@naver.com>
2022-07-31 13:56:25 +0900

    setting: init impl project

---
+++ 도메인 주도 개발 시작하기/impl/src/main/java/java/com/myshop/ImplApplication.java
@@ -0,0 +1,13 @@
+package java.com.myshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplApplication.class, args);
+    }
+
+}
---
+++ 도메인 주도 개발 시작하기/impl/src/test/java/java/com/myshop/ImplApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.myshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
